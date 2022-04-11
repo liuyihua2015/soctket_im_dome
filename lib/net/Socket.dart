2e78c21522645b382a01d4e51eb162968a0c79a2
liuyihua <liuyihua@shinsson.com>
2022-04-11 14:18:55 +0800

    基于Socket创建socket服务端和客户端

--- lib/net/Socket.dart
+++ lib/net/Socket.dart
@@ -1,10 +1,13 @@
 import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
-import 'package:soctket_im_dome/model/Message.dart';
+import '../model/Message.dart';
 
 class BaseSocketCS {
   var msgStream = StreamController<Message>();
+
   void init() async {}
 
   void send(Message msg) {}
@@ -13,4 +16,72 @@ class BaseSocketCS {
   void dispose() {
     msgStream.close();
   }
+}
+
+//基于Socket创建socket服务器端
+class SocketServer extends BaseSocketCS {
+  late ServerSocket serverSocket;
+  List<Socket> clients = [];
+  late int port;
+
+  SocketServer(this.port);
+
+  @override
+  void init() async {
+    ServerSocket.bind(InternetAddress.anyIPv4, port).then((bindSocket) {
+      serverSocket = bindSocket;
+      serverSocket.listen((clientSocket) {
+        utf8.decoder.bind(clientSocket).listen((data) {
+          msgStream.add(Message.fromJson(json.decode(data)));
+        });
+        clients.add(clientSocket);
+      });
+    });
+  }
+
+  @override
+  void send(Message msg) async {
+    for (var client in clients) {
+      client.add(utf8.encode(json.encode(msg)));
+    }
+  }
+
+  @override
+  void dispose() async {
+    super.dispose();
+    for (var socket in clients) {
+      socket.close();
+    }
+    serverSocket.close();
+  }
+}
+
+//基于Socket创建socket客户端
+class SocketClient extends BaseSocketCS {
+  late Socket clientSocket;
+  late String address;
+  late int port;
+
+  SocketClient(this.address, this.port);
+
+  @override
+  void init() async {
+    clientSocket = await Socket.connect(address, port) as Socket;
+    utf8.decoder.bind(clientSocket).listen((data) {
+      msgStream.add(Message.fromJson(json.encode(data)));
+    });
+  }
+
+  @override
+  void send(Message msg) {
+    clientSocket.add(utf8.encode(json.encode(msg)));
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    clientSocket.close();
+  }
+
 }
